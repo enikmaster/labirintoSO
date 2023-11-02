774f2194bab562546edeb611850af4ee7439221f
diogo <diogofgf97@gmail.com>
2023-11-02 19:41:16 +0000

    fixes + ncurses done

--- jogoUIFunctions.c
+++ jogoUIFunctions.c
@@ -4,88 +4,37 @@
 
 
 int verificaComandoUI(char *comando, WINDOW *janelaBaixo) {
-    const char listaComandos[][TAMANHO_COMANDO] =
-            {"players", "msg", "exit"};
     comando[strlen(comando)] = '\0';
     for (int i = 0; i < strlen(comando); ++i) {
         comando[i] = tolower(comando[i]);
     }
-    // se for só uma palavra
-    if (strchr(comando, ' ') == NULL) {
-        for (int i = 0; i <= strlen((const char *) listaComandos); ++i) {
-            if (strcmp(comando, listaComandos[i]) == 0) {
-                /*switch (i) {
-                    case 0:
-                        printf("Comando %s válido\n", comando);
-                        fflush(stdin);
-                        return 1;
-                    case 2:
-                        printf("Comando %s válido\n", comando);
-                        fflush(stdin);
-                        return 0;
-                }*/
-                if (strcmp(comando, "msg")==0) {
-                    wprintw(janelaBaixo, "\n Comando %s inválido ", comando);
-                    fflush(stdin);
-                    return 0;
-                }
-                wprintw(janelaBaixo, "\n Comando %s válido ", comando);
-                fflush(stdin);
-                return (strcmp(comando, "exit") == 0) ? 1 : 0;
-            }
-        }
-        wprintw(janelaBaixo, "\n Comando %s inválido ", comando);
+    char comandoTemp[TAMANHO_COMANDO] = {'\0'};
+    char userToMessage[TAMANHO_COMANDO] = {'\0'};
+    char msgToSend[TAMANHO_COMANDO] = {'\0'};
+    int nArgumentos = sscanf(comando, "%s %s %s", comandoTemp, userToMessage, msgToSend);
+
+    if (strcmp(comando, "players") == 0) {
+        wprintw(janelaBaixo, "\n Comando %s válido ", comando);
         fflush(stdin);
         return 0;
     }
-    // se quer mandar mensagem
-    if (strchr(comando, ' ') != NULL) {
-        char comandoTemp[TAMANHO_COMANDO] = {'\0'};
-        char userToMessage[TAMANHO_COMANDO] = {'\0'};
-        char msgToSend[TAMANHO_COMANDO] = {'\0'};
-        char argAux[TAMANHO_COMANDO] = {'\0'};
-        int nArgumentos = sscanf(comando, "%s %s %s %s", comandoTemp, userToMessage, msgToSend, argAux);
-        if (nArgumentos > 3) {
-            wprintw(janelaBaixo, "\n Comando %s inválido ", comando);
-            fflush(stdin);
-            return 0;
-        }
-        if (strcmp(comandoTemp, "msg") == 0 && nArgumentos == 3) {
-            if (checkIfUserAtivo(userToMessage)) {}
+    else if (nArgumentos == 3 && strcmp(comandoTemp, "msg") == 0 && checkIfUserAtivo(userToMessage)) {
             wprintw(janelaBaixo, "\n Comando %s válido ", comando);
             fflush(stdin);
             return 0;
-        }
-        wprintw(janelaBaixo, "\n Comando %s inválido ", comando);
     }
+    else if (strcmp(comando, "exit") == 0) {
+        fflush(stdin);
+        return 1;
+    }
+
+    wprintw(janelaBaixo, "\n Comando %s inválido ", comando);
     fflush(stdin);
     return 0;
 }
 
 
-bool checkIfUserAtivo(char *userToMessage) { return false;}
-
-void desenhaMoldura(int alt, int comp, int linha, int coluna) {
-    --linha;
-    --coluna;
-    alt+=2;
-    comp+=2;
-    // desenha barras verticais laterais
-    for (int l=linha; l<=linha+alt-1; ++l) {
-        mvaddch(l, coluna, '|');            // MoVe para uma posição e ADDiciona um CHar lá
-        mvaddch(l, coluna+comp-1, '|');
-    }
-    // desenha as barras horizontais
-    for (int c=coluna; c<=coluna+comp-1; ++c) {
-        mvaddch(linha, c, '-');
-        mvaddch(linha+alt-1, c, '-');
-    }
-    // desenha os cantos
-    mvaddch(linha, coluna, '+');
-    mvaddch(linha, coluna+comp-1, '+');
-    mvaddch(linha+alt-1, coluna, '+');
-    mvaddch(linha+alt-1,coluna+comp-1,'+');
-}
+bool checkIfUserAtivo(char *userToMessage) { return true;}
 
 void desenhaMapa(WINDOW *janela, int tipo)
 {
@@ -142,7 +91,7 @@ void trataTeclado(WINDOW *janelaTopo, WINDOW *janelaBaixo)
         }
         else if (tecla == ' ')
         {
-            //wclear(janelaBaixo);  queremos scroll? acho que n
+            wclear(janelaBaixo); // queremos scroll? acho que n
             echo();
             wprintw(janelaBaixo, "\n #> ");
             wgetstr(janelaBaixo, comando);
